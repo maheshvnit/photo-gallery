c36fe160da73e1bac505498edf21527fb8f29f72
Mahesh Prasad <mahesh.vrce@gmail.com>
2019-06-29 16:15:57 +0530

    take picture set opacity

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -11,6 +11,14 @@ export class PhotoService {
   constructor(private camera: Camera) { };
 
   takePicture() {
+
+    let appRoot = document.getElementsByTagName("app-root")[0];
+
+    
+    //appRoot.style.opacity = 0.5;
+    appRoot.setAttribute("style","opacity:0.5;");
+
+
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -27,10 +35,12 @@ export class PhotoService {
     */
     this.camera.getPicture(options).then((imageData) => {
         // Add new photo to gallery
+        appRoot.setAttribute("style","opacity:1;");
         this.photos.unshift({
             data: 'data:image/jpeg;base64,' + imageData
         }); }, (err) => {
         // Handle error
+        appRoot.setAttribute("style","opacity:1;");
         console.log("Camera issue: " + err);
     });   
   }  
